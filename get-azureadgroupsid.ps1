bd5e638c5c47db755ce72af63b8c6995b2f84a4d
Tristan Tyson <tristanktyson@gmail.com>
2023-06-20 21:52:21 +0100

    Code formatting

--- get-azureadgroupsid.ps1
+++ get-azureadgroupsid.ps1
@@ -1,6 +1,6 @@
 ﻿<#PSScriptInfo
 
-.VERSION 1.3
+.VERSION 1.4
 
 .GUID 4da60fad-3ad2-47f3-a26d-7dcbe0b8988d
 
@@ -9,8 +9,8 @@
 .TAGS AzureAD AzureAD-Group-SID
 
 .RELEASENOTES
-    11/18/22 - Initial release to PSGallery
-
+    Version 1.3 - Initial release to PSGallery.
+    Version 1.4 - Updated module to use MSgraph.
 #>
 
 <#
@@ -23,47 +23,57 @@
 #>
 Param(
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [string[]] $groupName
-)      
-       
-# Get NuGet
-$provider = Get-PackageProvider NuGet -ErrorAction Ignore
-if (-not $provider) {
-    Write-Host "Installing provider NuGet..."
-    Find-PackageProvider -Name NuGet -ForceBootstrap -IncludeDependencies
-}
+)
 
-$authModule = get-installedmodule -name Microsoft.Graph.Authentication -ErrorAction Ignore
-if (-not $authmodule) {
-    Write-Host "Installing Microsoft Graph Module..."
-    Install-Module Microsoft.Graph.Authentication -Force
-}
+try {
+    # Get NuGet provider
+    $provider = Get-PackageProvider NuGet -ErrorAction Ignore
+    if (-not $provider) {
+        Write-Host "Installing provider NuGet..."
+        Find-PackageProvider -Name NuGet -ForceBootstrap -IncludeDependencies
+    }
 
+    # Install Microsoft Graph Authentication Module
+    $authModule = get-installedmodule -name Microsoft.Graph.Authentication -ErrorAction Ignore
+    if (-not $authmodule) {
+        Write-Host "Installing Microsoft Graph Module..."
+        Install-Module Microsoft.Graph.Authentication -Force
+    }
 
-        
-# Connect to Graph
-$graph = Connect-MgGraph -scope 'Group.Read.All' 
-Write-Host "Connected to Microsoft Graph$($graph.TenantId)"
+    # Install Microsoft Graph Groups Module
+    $groupModule = get-installedmodule -name Microsoft.Graph.Groups -ErrorAction Ignore
+    if (-not $groupModule) {
+        Write-Host "Installing Microsoft Graph Module..."
+        Install-Module Microsoft.Graph.Groups -Force
+    }
 
-# Get AzureAD Group Object
-$groupInfo = Get-MgGroup -Filter "DisplayName eq '$groupName'"
-$groupObjectID = $groupInfo.Id
+    # Connect to Graph
+    $graph = Connect-MgGraph -scope 'Group.Read.All' 
+    Write-Host "Connected to Microsoft Graph$($graph.TenantId)"
 
-if ($groupObjectID) {
+    # Get AzureAD Group Object
+    $groupInfo = Get-MgGroup -Filter "DisplayName eq '$groupName'"
+    $groupObjectID = $groupInfo.Id
+    
     # Convert the ObjectID to SID
-    Write-Verbose "Converting ObjectID to SID"
-    $bytes = [Guid]::Parse($groupObjectID).ToByteArray()
-    $array = New-Object 'UInt32[]' 4
-    [Buffer]::BlockCopy($bytes, 0, $array, 0, 16)
-    $sid = "S-1-12-1-$array".Replace(' ', '-')
+    if ($groupObjectID) {
+        Write-Verbose "Converting ObjectID to SID"
+        $bytes = [Guid]::Parse($groupObjectID).ToByteArray()
+        $array = New-Object 'UInt32[]' 4
+        [Buffer]::BlockCopy($bytes, 0, $array, 0, 16)
+        $sid = "S-1-12-1-$array".Replace(' ', '-')
+    }
+    else {
+        throw "Unable to find group: $groupName"
+    }
+    # Create Object
+    $azureADGroupSID = [PSCustomObject]@{
+        Name     = $groupObject.DisplayName
+        sid      = $sid
+        ObjectID = $ObjectId
+    }
+    $azureADGroupSID.sid
 }
-else {
-    throw "Unable to find group: $groupName"
+catch {
+    throw $_
 }
-
-# Create Object
-$azureADGroupSID = [PSCustomObject]@{
-    Name     = $groupObject.DisplayName
-    sid      = $sid
-    ObjectID = $ObjectId
-}
-$azureADGroupSID.sid
